05bcbba1a42873d46fccca034d7bc5535aa54ffe
yscialom <yankel.scialom@mail.com>
2013-10-25 01:52:17 +0200

    update Progressive jobboard
    This patch updates the Progressive jobboard accordingly to the new rss feed
    found on the Progressive Recruitment web site <http://www.\
    progressiverecruitment.com/fr/job-search?task=searchAdverts&form-keywords=*\
    &form-job-type=2&form-sector=0&form-country=69&form-jobs-salary=0\
    &form-order-field=&form-order-dir=DESC&redirect=0&format=feed&type=rss>.
    
    The job location, salary and type (CDI/CDD/...) have vanished.

--- jobboards/Progressive.py
+++ jobboards/Progressive.py
@@ -42,8 +42,9 @@ class Progressive(Jobboard):
         fileObj.close()
         #xmldoc = minidom.parse(xmlfile)
 
-        MainPubDate = xmldoc.getElementsByTagName('posted')[0].firstChild.data
-        epochPubDate = datetime.datetime.strptime(MainPubDate, "%d/%m/%Y %H:%M:%S").strftime('%s')
+        MainPubDate = xmldoc.getElementsByTagName('lastBuildDate')[0].firstChild.data
+        MainPubDate = MainPubDate[:MainPubDate.rindex(' ')]
+        epochPubDate = datetime.datetime.strptime(MainPubDate, "%a, %d %b %Y %H:%M:%S").strftime('%s')
 
         if (epochPubDate <= self.lastFetchDate):
             return 0
@@ -54,7 +55,8 @@ class Progressive(Jobboard):
             # TODO : Test object first
             title = elt.getElementsByTagName('title')[0].firstChild.data
             link = elt.getElementsByTagName('link')[0].firstChild.data.split("?")[0] + "index.html"
-            pubDate = elt.getElementsByTagName('posted')[0].firstChild.data
+            pubDate = elt.getElementsByTagName('pubDate')[0].firstChild.data
+            pubDate = pubDate[:pubDate.rindex(' ')]
 
             if (epochPubDate <= self.lastFetchDate):
                 break
@@ -70,21 +72,19 @@ class Progressive(Jobboard):
             offer.lon = loc.lon
             offer.title = title.encode( 'iso-8859-1' )
             offer.url = link
-            offer.date_pub = datetime.datetime.strptime(pubDate, "%d/%m/%Y %H:%M:%S").strftime('%s')
+            offer.date_pub = datetime.datetime.strptime(pubDate, "%a, %d %b %Y %H:%M:%S").strftime('%s')
             offer.content = elt.getElementsByTagName('description')[0].firstChild.data
             offer.content = offer.content.encode( 'iso-8859-1' )
 
             offer.company = 'Progressive Recruitment'
 
-            offer.location = elt.getElementsByTagName('location')[0].firstChild.data
-            offer.location = offer.location.encode( 'iso-8859-1' )
+            offer.location = 'NA'
             offer.cleanLocation()
 
-            offer.contract = elt.getElementsByTagName('job_type')[0].firstChild.data
+            offer.contract = 'NA'
             offer.cleanContract()
 
-            offer.salary = elt.getElementsByTagName('salary')[0].firstChild.data
-            offer.salary = offer.salary.encode( 'iso-8859-1' )
+            offer.salary = 'NA'
             offer.cleanSalary()
 
             offer.experience = 'NA'
@@ -94,7 +94,7 @@ class Progressive(Jobboard):
     def fetch(self):
         print "Fetching " + self.name
 
-        feed_list = ['http://fr.progressiverecruitment.com/fr/JobSearch/Rss/France/CDI/69/2/0/0/0/1/index.html'] # ...Système, réseaux, donnée
+        feed_list = ['http://www.progressiverecruitment.com/fr/job-search?task=searchAdverts&form-keywords=*&form-job-type=2&form-sector=0&form-country=69&form-jobs-salary=0&form-order-field=&form-order-dir=DESC&redirect=0&format=feed&type=rss'] # ...Système, réseaux, donnée
 
         if (not os.path.isdir(self.processingDir)):
                 os.makedirs(self.processingDir)
