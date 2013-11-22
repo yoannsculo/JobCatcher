f0551356b77f6010707f8c56ebb95169054cf77f
badele <bruno.adele@jesuislibre.org>
2013-11-22 19:23:50 +0100

    Add exemples configurations

--- users/default.py
+++ users/default.py
@@ -13,29 +13,30 @@ __version__ = '0.1'
 configs = {
     'PoleEmploi': {
         'feeds': [
-            # {
-            #     'url': 'http://candidat.pole-emploi.fr/candidat/rechercheoffres/avancee.recherche',
-            #     'datas': {
-            #         'appelations': u'11128',
-            #         'boutonValider': u'',
-            #         'boutonValider:hiddenelementSubmit': u'boutonValider:hiddenelementSubmit',
-            #         't:formdata': u'H4sIAAAAAAAAAO2XP08CMRjGC4mLgDEObg4mDJqYnggmChqCgzgQNV507x0Fjty1te0BLk76Afwc/knUnRhHvouLuprYclEYBZWplzTXe3tPf8+zve/NC5hqF8Amx24Dc7VorcaxsFALERdjy8Y+dqVHSYkx7PtIb/NosBeCgw3K6xAxpNRQIoaF5Gfr0KUc+56j3gGjBBMp4BGqerTMacjSNpYhu1rtrjzOvD3HQawCEi4lklN/HwVYgrlKU1mwfETqli25R+qFDgezugh1ER44TWUMDJ4OkyA57Oy3sXJjxMr0Or3eg/3RjStDp+AcxNtZkBnJhU8pU3T4U3pF/Z9ee2W15fL8dXHC3OzT0sVe9v0WTJib+5u8Wjk9tjJpmIZpmIZpmIZpmIZpmIb5b8wSKI6kdGgo1SDzdUQwV132bo1DhmHg6o46CMl3qw29RjDcZe/01XboBJ5MH3LqYiH6X0Koy44v7++2E4sL0eCmEIES6cFNW41FDbma4SIL9pCF9hbIj5GihXyvqgLoi1MSpKLqSVT9BEr0aBPDDgAA',
-            #     }
-            # }
+            {
+                'url': 'http://candidat.pole-emploi.fr/candidat/rechercheoffres/avancee.recherche',
+                'datas': {
+                    'appelations': u'10316',
+                    'boutonValider': u'',
+                    'boutonValider:hiddenelementSubmit': u'boutonValider:hiddenelementSubmit',
+                    't:formdata': 'H4sIAAAAAAAAAO1VP0sDMRxNCy62FXFwcxA6KEjO2graKqUO1qGoeOieu+baK7kkJrm2Lk76Afwc/gF1L+LY7+KiroJJD21HW6UgNHDkd7/Ly3tvePe7eQFTrQLYFNitY6Ef5nkCSws1EXUxtmxMsKt8RkucY0KQKfOoX0spwAYTNYg40mioEMdSibN16DKBie/oPeCMYqokPEJVn5UFC3naxirkV6udlceZt+c4iFVAwmVUCUb2UYAVmKs0tASLIFqzbCV8Wiu0BZg1TWia8MBpaGGgv9pcgeSgst/ayo1gK9Ntd7sP9kcnrgWdgnMQb2VBZigVhDGu2eFP2Sv6fHrtlXvL5fnr4ph5s09LF3vZ91swZt7c3/g1yOmRkckJ54TzX3KWQHEopMNCpX+KX58oFjqxu56AHMPANekMQvodW+jXg8HE7vTQdugEvkofCuZiKXtvUurLji/v77YTiwvRENAUgQaZIWCkxqJw63kQSbAHJLS2QH4EF01E/Ko2YC5OKZCKuidR9xMIE1r2DwcAAA==',
+                }
+            },
+            {
+                'url': 'http://candidat.pole-emploi.fr/candidat/rechercheoffres/avancee.recherche',
+                'datas': {
+                    'appelations': u'18258',
+                    'boutonValider': u'',
+                    'boutonValider:hiddenelementSubmit': u'boutonValider:hiddenelementSubmit',
+                    't:formdata': 'H4sIAAAAAAAAAO1VP0sDMRxNCy62FXFwcxA6KEjO2graKqUO1qGoeOieu+baK7kkJrm2Lk76Afwc/gF1L+LY7+KiroJJD21HW6UgNHDkd7/Ly3tvePe7eQFTrQLYFNitY6Ef5nkCSws1EXUxtmxMsKt8RkucY0KQKfOoX0spwAYTNYg40mioEMdSibN16DKBie/oPeCMYqokPEJVn5UFC3naxirkV6udlceZt+c4iFVAwmVUCUb2UYAVmKs0tASLIFqzbCV8Wiu0BZg1TWia8MBpaGGgv9pcgeSgst/ayo1gK9Ntd7sP9kcnrgWdgnMQb2VBZigVhDGu2eFP2Sv6fHrtlXvL5fnr4ph5s09LF3vZ91swZt7c3/g1yOmRkckJ54TzX3KWQHEopMNCpX+KX58oFjqxu56AHMPANekMQvodW+jXg8HE7vTQdugEvkofCuZiKXtvUurLji/v77YTiwvRENAUgQaZIWCkxqJw63kQSbAHJLS2QH4EF01E/Ko2YC5OKZCKuidR9xMIE1r2DwcAAA==',
+                }
+            }
+
         ]
     },
     'Apec': {
-        'feeds' : [
-            # { 'url': 'http://www.apec.fr/fluxRss/XML/OffresCadre_F101828.xml'}, # Commercial, Marketing (Lieu indifférent)
-            # { 'url': 'http://www.apec.fr/fluxRss/XML/OffresCadre_F101829.xml'}, # Communication, Création (Lieu indifférent)
-            # { 'url': 'http://www.apec.fr/fluxRss/XML/OffresCadre_F101830.xml'}, # Direction d'entreprise (Lieu indifférent)
-            # { 'url': 'http://www.apec.fr/fluxRss/XML/OffresCadre_F101831.xml'}, # Etudes, Recherche et Développement (Lieu indifférent)
-            # { 'url': 'http://www.apec.fr/fluxRss/XML/OffresCadre_F101832.xml'}, # Gestion, Finance, Administration (Lieu indifférent)
-            # { 'url': 'http://www.apec.fr/fluxRss/XML/OffresCadre_F101833.xml'}, # Informatique (Lieu indifférent)
-            # { 'url': 'http://www.apec.fr/fluxRss/XML/OffresCadre_F101834.xml'}, # Production Industrielle, Travaux, Chantiers (Lieu indifférent)
-            # { 'url': 'http://www.apec.fr/fluxRss/XML/OffresCadre_F101835.xml'}, # Ressources Humaines (Lieu indifférent)
-            # { 'url': 'http://www.apec.fr/fluxRss/XML/OffresCadre_F101836.xml'}, # Sanitaire, Social, Culture (Lieu indifférent)
-            # { 'url': 'http://www.apec.fr/fluxRss/XML/OffresCadre_F101837.xml'}, # Services Techniques (Lieu indifférent)
+        'feeds': [
+            { 'url': 'http://www.apec.fr/fluxRss/XML/OffresCadre_F101833_R711.xml'}
         ]
     },
     'cadresonline': {
@@ -45,14 +46,14 @@ configs = {
     },
     'RegionJob': {
         'feeds': [
-            {'url': 'http://www.pacajob.com/fr/rss/flux.aspx?ville=9&fonction=7&qualification=3'},
-            {'url': 'http://www.pacajob.com/fr/rss/flux.aspx?ville=9&fonction=9&qualification=3'},
-            {'url': 'http://www.pacajob.com/fr/rss/flux.aspx?ville=9&fonction=10&qualification=3'},
+            {'url': 'http://www.parisjob.com/fr/rss/flux.aspx?&fonction=7'},
+            {'url': 'http://www.parisjob.com/fr/rss/flux.aspx?&fonction=9'},
+            {'url': 'http://www.parisjob.com/fr/rss/flux.aspx?&fonction=10'},
         ]
     },
     'Eures': {
         'feeds': [
-            { 'url': 'https://ec.europa.eu/eures/eures-searchengine/servlet/BrowseCountryJVsServlet?lg=FR&isco=213&multipleCountries=FR-R281&date=01%2F01%2F1975&title=&durex=&exp=&serviceUri=browse&qual=&pageSize=99&page=1&country=FR&totalCount=1&multipleRegions=R281'}
+            #{ 'url': 'https://ec.europa.eu/eures/eures-searchengine/servlet/BrowseCountryJVsServlet?lg=FR&isco=213&multipleCountries=FR-R281&date=01%2F01%2F1975&title=&durex=&exp=&serviceUri=browse&qual=&pageSize=99&page=1&country=FR&totalCount=1&multipleRegions=R281'}
         ]
     }
 }
