yoannsculo/JobCatcher

commit e2c34f611452ccb12320be0a82f73af49117486a

Highlighting the filtered salaries

jc/report.py
@@ -320,8 +320,10 @@ class ReportGenerator(object):
                     report.write(self.box('', offer.contract))
                     report.write(duration)
                 report.write('</td>\n')
-                report.write('\t\t\t\t<td class="salary">' + offer.salary_cleaned + '</td>\n')
-
+                if offer.salary_cleaned == 'NA' or offer.salary == offer.salary_cleaned:
+                    report.write('\t\t\t\t<td class="salary">' + offer.salary_cleaned + '</td>\n')
+                else:
+                    report.write('\t\t\t\t<td class="salary">' + self.box('primary',offer.salary_cleaned + '</td>\n'))
                 # Source
                 feedurl = feedsinfo[offer.src][offer.feedid]['url']
                 report.write('\t\t\t\t<td class="source"><a href="%s">%s</a></td>\n' % \

commit c00bbdd643e411ecf0fed1983a5c73297c02ad71

Fix html bug introduced in commit e2c34f6.

jc/report.py
@@ -348,7 +348,7 @@ class ReportGenerator(object):
                 if offer.salary_cleaned == 'NA' or offer.salary == offer.salary_cleaned:
                     report.write('\t\t\t\t<td class="salary">' + offer.salary_cleaned + '</td>\n')
                 else:
-                    report.write('\t\t\t\t<td class="salary">' + self.box('primary',offer.salary_cleaned + '</td>\n'))
+                    report.write('\t\t\t\t<td class="salary">' + self.box('primary', offer.salary_cleaned) + '</td>\n')
                 # Source
                 feedurl = feedsinfo[offer.src][offer.feedid]['url']
                 report.write('\t\t\t\t<td class="source"><a href="%s">%s</a></td>\n' % \